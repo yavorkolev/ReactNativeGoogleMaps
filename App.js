3eab2778afe7bcef2721c6f0010decf9ffd03b55
yavorkolev <ygk@mail.bg>
2020-08-29 23:34:36 +0300

    Added: Runtime Permissions Request for Gps device

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, } from 'react-native';
+import { PermissionsAndroid, StyleSheet, View, } from 'react-native';
 
 import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
 
+export async function request_location_runtime_permission() {
+
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        'title': 'ReactNativeCode Location Permission',
+        'message': 'ReactNativeCode App needs access to your location '
+      }
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      Alert.alert("Location Permission Granted.");
+    }
+    else {
+      Alert.alert("Location Permission Not Granted");
+    }
+  } catch (err) {
+    console.warn(err)
+  }
+}
+
 export default class App extends Component{
+  async componentDidMount() {
+    await request_location_runtime_permission()
+  }
   render(){
     return (
     <View style = {styles.container}>
